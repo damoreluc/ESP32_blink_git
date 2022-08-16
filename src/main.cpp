b1f942dce96d9272a7a2b6c0696b9178dbe5b420
damoreluc <damore.luca@isisbassafriulana.edu.it>
2022-08-16 17:06:06 +0200

    Aggiunto led pin 16

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 
 void setup() {
   pinMode(15, OUTPUT);
+  pinMode(16, OUTPUT);  
 }
 
 void loop() {
   digitalWrite(15, HIGH);
+  digitalWrite(16, LOW);  
   delay(500);
   digitalWrite(15, LOW);
+  digitalWrite(16, HIGH);  
   delay(500);  
 }
