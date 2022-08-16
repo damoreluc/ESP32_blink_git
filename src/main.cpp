43de889810b7624d69c81be6370b1b557657c824
damoreluc <damore.luca@isisbassafriulana.edu.it>
2022-08-16 16:32:06 +0200

    aggiunto lampeggio led sul pin 15

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(15, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(15, HIGH);
+  delay(500);
+  digitalWrite(15, LOW);
+  delay(500);  
 }
